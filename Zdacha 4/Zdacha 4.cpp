cb3e0f9666ee502912c9c4ab23036c0bec573b5f
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-02 00:30:26 +0200

    Добавьте файлы проекта.

---
+++ Zdacha 4/Zdacha 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    int A, B, C;
+    cout << "write A, B и C: ";
+    cin >> A >> B >> C;
+
+    if (A < B && B < C) {
+        cout << "C-B-A = " << C - B - A << endl;
+    }
+    else if (A % C == 0) {
+        cout << "A/C+B = " << A / C + B << endl;
+    }
+    else {
+        cout << "A+B+C = " << A + B + C << endl;
+    }
+
+    return 0
+}
+
+
